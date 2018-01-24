97d470975f71a41afdf9c98b9558c74be9a14083
bmsleft <bmsleft@163.com>
2018-01-24 20:14:38 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,4 +1,3 @@
-
 '''
 For RaspberryPi
 it will capture a picture from camera on RaspberryPi every 1(default) minute,
@@ -6,14 +5,15 @@ and upload it to Baidu netdisk
 Make sure you can use bypy normally.(https://github.com/houtianze/bypy)
 By bms
 '''
+import os
 from time import sleep
-from datatime import datatime, timedelta
+from datetime import datetime, timedelta
 from picamera import PiCamera
 from bypy import ByPy
 
 def wait(delay_minute = 1):
-    next_time = (datatime.now() + timedelta(minute=delay_minute)).replace(second=0, microsecond=0)
-    delay = (next_time - datatime.now()).seconds
+    next_time = (datetime.now() + timedelta(minutes=delay_minute)).replace(second=0, microsecond=0)
+    delay = (next_time - datetime.now()).seconds
     sleep(delay)
 
 by=ByPy()
@@ -21,9 +21,8 @@ camera = PiCamera()
 camera.start_preview()
 wait()
 
-for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg')
+for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
     print('capture %s' % filename)
     by.upload(filename)
+    os.remove(filename)
     wait()
-
-
